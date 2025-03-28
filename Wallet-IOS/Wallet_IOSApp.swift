ee992633f0c46e3a608315f7482e1426e7690a31
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-28 21:25:07 +0100

    Initial Commit

---
+++ Wallet-IOS/Wallet_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_IOSApp.swift
+//  Wallet-IOS
+//
+//  Created by Daria Kozlovska on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
